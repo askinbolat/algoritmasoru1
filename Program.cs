6cc3b869e98cfdabfe7ac4b9b1ef7c975849b810
Aşkın <askinbolat@gmail.com>
2022-10-14 19:02:20 +0300

    algoritma soru 1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+// Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+// Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+// Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+namespace Algoritma_soruları_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            int[] sayidizisi = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} . pozitif sayıyı giriniz:",i+1);
+                sayidizisi [i]=int.Parse(Console.ReadLine());
+
+            }
+
+            foreach (var item in sayidizisi)
+            {
+                if(item%2==0)
+                    Console.WriteLine(item);
+                else
+                    Console.WriteLine();
+
+                
+
+            }
+        
+    
+            
+            
+
+            
+
+
+        }
+
+    }
+}
